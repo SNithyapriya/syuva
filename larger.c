d194518692495907e11f1330591bb405b30a7783
SNithyapriya <69196056+SNithyapriya@users.noreply.github.com>
2020-08-04 16:21:12 +0530

    Update and rename README.md to larger.c

---
+++ larger.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d\n",&a);
+    scanf("%d\n",&b);
+    scanf("%d\n",&c);
+    if(a>b&&a>c)
+    {
+        printf("%d",a);
+        
+    }
+    else if(b>c)
+    {
+        printf("%d",b);
+    }
+    else
+    {
+        printf("%d",c);
+    }
+    return 0;
+}
